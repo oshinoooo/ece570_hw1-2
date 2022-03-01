d996ac50e31a4f43ac58ad4e0d7a8d58ecf41a4c
oshinoooo <1036858975@qq.com>
2022-03-01 13:34:12 -0500

    2022-03-01 13:34:12

--- thread.cc
+++ thread.cc
@@ -1,10 +1,44 @@
-#include "thread.h"
 #include <iostream>
+#include <ucontext.h>
+
+#include "thread.h"
 
 using namespace std;
 
+// getcontext
+// makecontext
+// swapcontext
+
+
+
+///*
+// * Initialize a context structure by copying the current thread's context.
+// */
+//getcontext(ucontext_ptr);           // ucontext_ptr has type (ucontext_t *)
+//
+///*
+// * Direct the new thread to use a different stack.  Your thread library
+// * should allocate STACK_SIZE bytes for each thread's stack.
+// */
+//char *stack = new char [STACK_SIZE];
+//ucontext_ptr->uc_stack.ss_sp = stack;
+//ucontext_ptr->uc_stack.ss_size = STACK_SIZE;
+//ucontext_ptr->uc_stack.ss_flags = 0;
+//ucontext_ptr->uc_link = NULL;
+//
+///*
+// * Direct the new thread to start by calling start(arg1, arg2).
+// */
+//makecontext(ucontext_ptr, (void (*)()) start, 2, arg1, arg2);
+
 int thread_libinit(thread_startfunc_t func, void *arg) {
-    return 0;
+
+
+
+
+
+    cout << "Thread library exiting." << endl;
+    exit(0);
 }
 
 int thread_create(thread_startfunc_t func, void *arg) {
