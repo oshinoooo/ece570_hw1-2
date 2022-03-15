e5672061bf99b50ac58dabc7c5c3f644870fc9da
oshinoooo <1036858975@qq.com>
2022-03-15 15:53:00 -0400

    2022-03-15 15:53:00

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ disk.cc lib/libinterrupt.a -ldl -o disk
+g++ -m32 disk.cc lib/libinterrupt.a -ldl -o disk
 
 ./disk 3 TestCases/disk.in0 TestCases/disk.in1 TestCases/disk.in2 TestCases/disk.in3 TestCases/disk.in4
--- thread.cc
+++ thread.cc
@@ -2,34 +2,9 @@
 #include <ucontext.h>
 
 #include "thread.h"
+#include "interrupt.h"
 
 using namespace std;
-
-// getcontext
-// makecontext
-// swapcontext
-
-
-
-///*
-// * Initialize a context structure by copying the current thread's context.
-// */
-//getcontext(ucontext_ptr);           // ucontext_ptr has type (ucontext_t *)
-//
-///*
-// * Direct the new thread to use a different stack.  Your thread library
-// * should allocate STACK_SIZE bytes for each thread's stack.
-// */
-//char *stack = new char [STACK_SIZE];
-//ucontext_ptr->uc_stack.ss_sp = stack;
-//ucontext_ptr->uc_stack.ss_size = STACK_SIZE;
-//ucontext_ptr->uc_stack.ss_flags = 0;
-//ucontext_ptr->uc_link = NULL;
-//
-///*
-// * Direct the new thread to start by calling start(arg1, arg2).
-// */
-//makecontext(ucontext_ptr, (void (*)()) start, 2, arg1, arg2);
 
 int thread_libinit(thread_startfunc_t func, void *arg) {
 
@@ -50,10 +25,23 @@ int thread_yield(void) {
 }
 
 int thread_lock(unsigned int lock) {
+
+
+
+
+
+
+
     return 0;
 }
 
 int thread_unlock(unsigned int lock) {
+
+
+
+
+
+
     return 0;
 }
 
