0e5651cb49e1344879f9a6b6f052abdf34a88ed1
oshinoooo <1036858975@qq.com>
2022-03-15 17:15:11 -0400

    2022-03-15 17:15:11

--- thread.cc
+++ thread.cc
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <sys/ucontext.h>
 //#include <ucontext.h>
-
 #include "thread.h"
 #include "interrupt.h"
 
 using namespace std;
 
+static bool lib_initialized = false;
+
+queue<ucontext_t*> ready;
+queue<ucontext_t*> waite;
+
 int thread_libinit(thread_startfunc_t func, void* arg) {
-    static int init = 0;
-    if (init == 0)
-        init = 1;
+    if (!lib_initialized) {
+        lib_initialized = true;
+    }
     else {
-        cout << "Thread library should only be initialed once." << endl;
+        cout << "Thread library should only be initialized once." << endl;
         return -1;
     }
 
@@ -22,15 +29,36 @@ int thread_libinit(thread_startfunc_t func, void* arg) {
 }
 
 int thread_create(thread_startfunc_t func, void* arg) {
+    if (!lib_initialized) {
+        cout << "Thread library should be initialized first." << endl;
+        return -1;
+    }
+
+    
+
+
     return 0;
 }
 
 int thread_yield(void) {
+    if (!lib_initialized) {
+        cout << "Thread library should be initialized first." << endl;
+        return -1;
+    }
+
+
+
+
+
+
     return 0;
 }
 
 int thread_lock(unsigned int lock) {
-
+    if (!lib_initialized) {
+        cout << "Thread library should be initialized first." << endl;
+        return -1;
+    }
 
 
 
@@ -41,7 +69,10 @@ int thread_lock(unsigned int lock) {
 }
 
 int thread_unlock(unsigned int lock) {
-
+    if (!lib_initialized) {
+        cout << "Thread library should be initialized first." << endl;
+        return -1;
+    }
 
 
 
@@ -51,17 +82,52 @@ int thread_unlock(unsigned int lock) {
 }
 
 int thread_wait(unsigned int lock, unsigned int cond) {
+    if (!lib_initialized) {
+        cout << "Thread library should be initialized first." << endl;
+        return -1;
+    }
+
+
+
+
+
     return 0;
 }
 
 int thread_signal(unsigned int lock, unsigned int cond) {
+    if (!lib_initialized) {
+        cout << "Thread library should be initialized first." << endl;
+        return -1;
+    }
+
+
+
+
     return 0;
 }
 
 int thread_broadcast(unsigned int lock, unsigned int cond) {
+    if (!lib_initialized) {
+        cout << "Thread library should be initialized first." << endl;
+        return -1;
+    }
+
+
+
+
+
     return 0;
 }
 
 void start_preemptions(bool async, bool sync, int random_seed) {
+    if (!lib_initialized) {
+        cout << "Thread library should be initialized first." << endl;
+        return;
+    }
+
+
+
+
+
     return;
 }
