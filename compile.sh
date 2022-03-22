5e134bc6eb66781fa08a1416ff1d7748c089eed9
oshinoooo <1036858975@qq.com>
2022-03-22 14:00:32 -0400

    2022-03-22 14:00:32

--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+scp -P 2500 thread.cc test1.cc group28@localhost:Desktop/.
+
+submit570 1t Desktop/thread.cc Desktop/test1.cc
+
 g++ -m32 thread.cc test1.cc lib/libinterrupt.a -ldl -o test
 
 ./disk 3 TestCases/disk.in0 TestCases/disk.in1 TestCases/disk.in2 TestCases/disk.in3 TestCases/disk.in4
