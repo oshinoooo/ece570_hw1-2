3f834c4dd6d38ed70182d4d80b19e25496102bd1
oshinoooo <1036858975@qq.com>
2022-03-21 18:37:52 -0700

    2022-03-21 18:37:52

--- compile.sh
+++ compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-g++ -m32 thread.cc test.cc lib/libinterrupt.a -ldl -o test
-
-g++ -m32 thread.cc disk.cc lib/libinterrupt.a -ldl -o disk
+g++ -m32 thread.cc test1.cc lib/libinterrupt.a -ldl -o test
 
 ./disk 3 TestCases/disk.in0 TestCases/disk.in1 TestCases/disk.in2 TestCases/disk.in3 TestCases/disk.in4
