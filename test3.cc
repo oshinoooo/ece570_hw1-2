93819dd0ffaba875334af15de9c2ff67b2e81a90
oshinoooo <1036858975@qq.com>
2022-03-23 15:42:41 -0400

    2022-03-23 15:42:41

---
+++ test3.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "thread.h"
+
+using namespace std;
+
+void show(void* ptr) {
+    for (int i = 0; i < 10; ++i)
+        cout << (long)ptr << " ";
+    cout << endl;
+}
+
+void start(void* ptr) {
+    for (long i = 0; i < 3; ++i) {
+        long index = i;
+        thread_create(show, (void*)index);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    thread_libinit(start, nullptr);
+    return 0;
+}
+
+
+// double lock
+// double everything
