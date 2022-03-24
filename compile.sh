b7846194c10bd7ce6e896657076c5c5483255286
oshinoooo <1036858975@qq.com>
2022-03-24 11:58:03 -0400

    2022-03-24 11:58:03

--- compile.sh
+++ compile.sh
@@ -2,8 +2,6 @@
 
 scp -P 2500 thread.cc test1.cc group28@localhost:Desktop/.
 
-submit570 1t Desktop/thread.cc Desktop/test1.cc
+submit570 1t src/thread.cc tests/*
 
-g++ -m32 thread.cc test1.cc lib/libinterrupt.a -ldl -o test
-
-./disk 3 TestCases/disk.in0 TestCases/disk.in1 TestCases/disk.in2 TestCases/disk.in3 TestCases/disk.in4
+g++ -m32 src/thread.cc tests/test1.cc lib/libinterrupt.a -ldl -o test
