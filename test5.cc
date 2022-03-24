97cad9f87906d6e8a00670bcc897f190d1a94b71
oshinoooo <1036858975@qq.com>
2022-03-24 11:42:50 -0400

    2022-03-24 11:42:50

--- test5.cc
+++ test5.cc
@@ -7,27 +7,37 @@ using namespace std;
 unsigned int lock = 1;
 
 void show(void* ptr) {
-    thread_unlock(lock);
-    thread_lock(lock);
+    int ret;
+    ret = thread_lock(lock);
+    if (ret == -1) {
+        cout << "Error in thread library." << endl;
+    }
+
     for (int i = 0; i < 100; ++i)
         cout << (long)ptr << " ";
     cout << endl;
-    thread_unlock(lock);
+
+    ret = thread_unlock(lock);
+    if (ret == -1) {
+        cout << "Error in thread library." << endl;
+    }
 }
 
 void start(void* ptr) {
     for (long i = 0; i < 5; ++i) {
-        thread_create(show, (void*)i);
+        int ret;
+        ret = thread_create(show, (void*)i);
+        if (ret == -1) {
+            cout << "Error in thread library." << endl;
+        }
     }
 }
 
 int main(int argc, char* argv[]) {
-    int ret = thread_libinit(start, nullptr);
+    int ret;
+    ret = thread_libinit(start, nullptr);
     if (ret == -1) {
         cout << "Thread library initialization failed." << endl;
-    }
-    else {
-        cout << "Thread library initialization succeeded." << endl;
     }
     return 0;
 }
