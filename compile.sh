cf7342d485103e26670756862e8e050325e0077c
oshinoooo <1036858975@qq.com>
2022-03-24 20:59:51 -0700

    2022-03-24 20:59:51

--- compile.sh
+++ compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-scp -P 2500 thread.cc test1.cc group28@localhost:Desktop/.
-
 submit570 1t src/thread.cc tests/*
 
 g++ -m32 src/thread.cc tests/test1.cc lib/libinterrupt.a -ldl -o test
