dbb19de498954e068c33754b14c4ddb6bdef4739
oshinoooo <1036858975@qq.com>
2022-03-24 21:05:46 -0700

    2022-03-24 21:05:46

--- compile.sh
+++ compile.sh
@@ -2,4 +2,8 @@
 
 submit570 1t src/thread.cc tests/*
 
-g++ -m32 src/thread.cc tests/test1.cc lib/libinterrupt.a -ldl -o test
+g++ -m32 -I./src src/thread.cc tests/test1.cc lib/libinterrupt.a -ldl -o test
+g++ -m32 -I./src src/thread.cc tests/test2.cc lib/libinterrupt.a -ldl -o test
+g++ -m32 -I./src src/thread.cc tests/test3.cc lib/libinterrupt.a -ldl -o test
+g++ -m32 -I./src src/thread.cc tests/test4.cc lib/libinterrupt.a -ldl -o test
+g++ -m32 -I./src src/thread.cc tests/test5.cc lib/libinterrupt.a -ldl -o test
